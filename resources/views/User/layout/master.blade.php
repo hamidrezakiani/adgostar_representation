a14b2c0aac8f89412f222b96b58bd8c1a1d8f9a8
hamidrezakiani <53411134+hamidrezakiani@users.noreply.github.com>
2022-03-03 23:51:59 +0330

    2

--- resources/views/User/layout/master.blade.php
+++ resources/views/User/layout/master.blade.php
@@ -60,7 +60,7 @@ input[type=number] {
           </section>
         </div> <!-- /.content-wrapper -->
         <footer class="main-footer">
-            <strong><a href="https://adgostar.net">ادگستر</a>.</strong>
+            <strong><a href="https://adgostar.net">ادگستر</a></strong>
         </footer>
 
           <!-- Control Sidebar -->
